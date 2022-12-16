b821ba2f9015699abbb927ff46d0c493dcff0bbc
Beyza Evcen <beyzaevcen37@icloud.com>
2022-12-16 14:28:44 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 void main() {
@@ -21,10 +22,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor:mobileBackgroundColor,
       ),
+      /*
       home: const ResponsiveLayout(
         mobileScreenLayout:MobileScreenLayout(),
         webScreenLayout:WebScreenLayout() ,
-      ),
+      ),      */
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 32.0),
+          width: double.infinity,
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+
+      )
+    );
+  }
+}
