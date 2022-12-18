ca6603652d1298bde746d75c918dbf77642a2902
Beyza Evcen <beyzaevcen37@icloud.com>
2022-12-18 23:11:38 +0300

    new

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/text_field_input.dart';
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  final TextEditingController _emailController=TextEditingController();
+  final TextEditingController _passwordController=TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +15,28 @@ class LoginScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 32.0),
           width: double.infinity,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Flexible(child: Container(),
+              flex: 2,
+              ),
+              //svg image
+              SvgPicture.asset(
+                 'assets/ic_instagram.svg',
+                  color: primaryColor,
+                  height: 64
+              ),
+              const SizedBox(height: 64),
+              //text field input for email
+              TextFieldInput(
+                  textEditingController:_emailController ,
+                  hintText: 'Enter your email',
+                  textInputType: TextInputType.emailAddress)
 
+
+              //text field input for password
+              //button login
+              //Transititioting to signing up
             ],
           ),
         ),
