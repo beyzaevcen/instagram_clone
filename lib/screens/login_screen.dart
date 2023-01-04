f9577c95626cfe4b125138c84415da3e97b365e0
Beyza Evcen <beyzaevcen37@icloud.com>
2023-01-04 22:16:41 +0300

    new

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,9 +17,7 @@ class LoginScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Flexible(child: Container(),
-              flex: 2,
-              ),
+              Flexible(child: Container(), flex: 2,),//I use this because i want to put insatgram logo at bottom
               //svg image
               SvgPicture.asset(
                  'assets/ic_instagram.svg',
@@ -31,12 +29,59 @@ class LoginScreen extends StatelessWidget {
               TextFieldInput(
                   textEditingController:_emailController ,
                   hintText: 'Enter your email',
-                  textInputType: TextInputType.emailAddress)
-
-
+                  textInputType: TextInputType.emailAddress
+              ),
+              const SizedBox(height: 20),
               //text field input for password
+              TextFieldInput(
+                  textEditingController: _passwordController,
+                  hintText: 'Enter your password',
+                  textInputType: TextInputType.text,
+                isPass: true,
+              ),
+
+              const SizedBox(height: 20),
+
               //button login
+              InkWell(
+                child: Container(
+                  child: const Text('Log in'),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(shape: RoundedRectangleBorder(
+                      borderRadius:BorderRadius.all(
+                        Radius.circular(4),
+                      ),
+                  ),
+                    color: blueColor,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 12),
+              Flexible(child: Container(), flex: 2,),
+
               //Transititioting to signing up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: const Text("Don't have an account?"),
+                    padding: const EdgeInsets.symmetric(vertical: 8,),
+                  ),
+                  GestureDetector(
+                    onTap: (){},
+                    child: Container(
+                      child: const Text(
+                        "Sign up",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
